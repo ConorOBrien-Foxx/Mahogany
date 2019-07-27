791fc5aa3c7d2e57a5c642b9dda2928cfebd2004
ConorOBrien-Foxx <conorobrien4god@gmail.com>
2019-07-27 00:10:06 -0400

    init bg test

--- mahogany.js
+++ mahogany.js
@@ -1,36 +1,58 @@
 //z
-class GameInstance {
+const Draw = {
+    clear: function (ctx) {
+        ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    }
+}
 
-};
-class Map {
 
-};
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-            }
+class Renderable {
+    constructor() {
+
+    }
+
+    render(ctx) {
+
+    }
+}
+
+class GameInstance extends Renderable {
+    constructor() {
+        super();
+        this.background = null;
+        this.objects = [];
+    }
+
+    render(ctx) {
+        Draw.clear(ctx);
+
+        if(this.background) {
+            ctx.drawImage(this.background, 0, 0);
+        }
+
+        for(let object of this.objects) {
+            object.render(ctx);
         }
     }
-    rawFile.send(null);
-}
+};
 
 window.addEventListener("load", function () {
     const game = document.getElementById("game");
+    const ctx = game.getContext("2d");
     const width = 960;
     const height = 540;
 
     game.width = width;
     game.height = height;
 
+    const instance = new GameInstance(ctx);
+    fetch("./map-01.png")
+        .then(img => { instance.background = img });
+
+    let render = function () {
+        instance.render(ctx);
+    }
+    let renderInterval = setInterval(render, 100);
     window.addEventListener("keydown", function (ev) {
         console.log(ev);
     })
